19d755dcc034a9401d415aaf0e23bf637572b620
LuciferTheFirstt <dimonator67@gmail.com>
2019-06-24 15:16:31 +0300

    added third-party

---
+++ _build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/luciferthefirstt/LuciferTheFirstt/workspace/workspace/projects/lab05
+# Build directory: /home/luciferthefirstt/LuciferTheFirstt/workspace/workspace/projects/lab05/_build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/luciferthefirstt/LuciferTheFirstt/workspace/workspace/projects/lab05/_build/check")
+subdirs("third-party/gtest")
---
+++ _build/third-party/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/luciferthefirstt/LuciferTheFirstt/workspace/workspace/projects/lab05/third-party/gtest/googlemock
+# Build directory: /home/luciferthefirstt/LuciferTheFirstt/workspace/workspace/projects/lab05/_build/third-party/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
