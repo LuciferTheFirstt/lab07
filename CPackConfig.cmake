f79db7619c8b537821cf37866850bc79ad2ff452
LuciferTheFirstt <dimonator67@gmail.com>
2019-09-03 23:44:43 +0300

    added cpack config

---
+++ CPackConfig.cmake
@@ -0,0 +1,14 @@
+include(InstallRequiredSystemLibraries)
+
+set(CPACK_RPM_PACKAGE_NAME "print-devel")
+set(CPACK_RPM_PACKAGE_LICENSE "MIT")
+set(CPACK_RPM_PACKAGE_GROUP "print")
+set(CPACK_RPM_CHANGELOG_FILE ${CMAKE_CURRENT_SOURCE_DIR}/ChangeLog.md)
+set(CPACK_RPM_PACKAGE_RELEASE 1)  # номер релиза RPM пакета
+set(CPACK_DEBIAN_PACKAGE_NAME "libprint-dev")
+set(CPACK_DEBIAN_PACKAGE_PREDEPENDS "cmake >= 3.0")
+set(CPACK_DEBIAN_PACKAGE_RELEASE 1) 
+set(CPACK_DMG_VOLUME_NAME "libprint-d")
+
+
+include(CPack)
---
+++ ChangeLog.md
@@ -0,0 +1,2 @@
+* Tue Sep 03 2019 LuciferTheFirstt <dimonator67@gmail.com> 0.1.0.0
+- Initial RPM release
