f95d275b59d2db0483a10d5b971fb017382955d6
LuciferTheFirstt <dimonator67@gmail.com>
2019-09-03 21:52:44 +0300

    added tests

---
+++ _build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/luciferthefirstt/LuciferTheFirstt/workspace/workspace/projects/laba05
+# Build directory: /home/luciferthefirstt/LuciferTheFirstt/workspace/workspace/projects/laba05/_build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/luciferthefirstt/LuciferTheFirstt/workspace/workspace/projects/laba05/_build/check")
+subdirs("third-party/gtest")
